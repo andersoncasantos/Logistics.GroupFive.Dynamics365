a253aedcef57fe280420a71d738696ffd4a9c486
andersoncasantos <andersonmtv07@gmail.com>
2024-02-26 16:45:24 -0300

    Function OnChangeVisibilityCnpjCpf

--- Logistics.GroupFive.Customizations/WebResourceDynamics1/Logistics1.Dynamics365.Account.js
+++ Logistics.GroupFive.Customizations/WebResourceDynamics1/Logistics1.Dynamics365.Account.js
@@ -5,4 +5,29 @@ if (typeof (LogisticsOne.Account) === "undefined") LogisticsOne.Account = {};
 
 LogisticsOne.Account = {
 
+	Enumerador: {
+		Tipo: {
+			PessoaFisica: 748910000,
+			PessoaJuridica: 748910001,
+			Internacional: 748910002
+		}
+	},
+
+	OnChangeVisibilityCnpjCpf: function (context) {
+		let formContext = context.getFormContext();
+		let tipoid = formContext.getAttribute("alf_tipo").getValue();
+
+		if (tipoid === LogisticsOne.Account.Enumerador.Tipo.PessoaJuridica) {
+			formContext.getControl("alf_cnpj").setVisible(true);
+			formContext.getControl("alf_cpf").setVisible(false);
+		}
+		else if (tipoid === LogisticsOne.Account.Enumerador.Tipo.PessoaFisica) {
+			formContext.getControl("alf_cpf").setVisible(true);
+			formContext.getControl("alf_cnpj").setVisible(false);
+		}
+		else if (tipoid === LogisticsOne.Account.Enumerador.Tipo.Internacional) {
+			formContext.getControl("alf_cpf").setVisible(false);
+			formContext.getControl("alf_cnpj").setVisible(false);
+		}
+	},
 }
