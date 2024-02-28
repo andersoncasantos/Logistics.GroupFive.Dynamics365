f2bd1483511b3ace611ee3d6a97062e17155505b
andersoncasantos <andersonmtv07@gmail.com>
2024-02-27 22:35:53 -0300

    starting activity two

---
+++ Logistics.GroupFive.PluginsDyn2/Plugins/ProductManager.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logistics.GroupFive.PluginsDyn2.Plugins
+{
+    public class ProductManager : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            ITracingService trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            Entity product = (Entity)context.InputParameters["Target"];
+            bool productIsIntegration = (bool)product["dyn2_isintegration"];
+
+            if (!productIsIntegration)
+            {
+                throw new InvalidPluginExecutionException("Um produto não pode ser cadastrado diretamente no Dynamics 2");
+            }
+        }
+    }
+}
